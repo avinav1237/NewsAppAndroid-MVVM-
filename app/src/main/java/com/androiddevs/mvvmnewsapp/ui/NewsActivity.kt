965de9b5aa841cf94a5fc0f2d3fc3b5182647108
Avinav Srivastava <avinav.ku22@gmail.com>
2023-01-27 21:59:59 +0530

    added the navigation

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -2,12 +2,17 @@ package com.androiddevs.mvvmnewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.mvvmnewsapp.R
+import kotlinx.android.synthetic.main.news_activity_main.*
 
 class NewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.news_activity_main)
+
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
