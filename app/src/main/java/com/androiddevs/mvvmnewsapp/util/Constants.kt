bfdb3dc8316651e2e52cf2ea0a003d3e7354f56e
Avinav Srivastava <avinav.ku22@gmail.com>
2023-01-29 13:41:13 +0530

    added the DAO

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "a0a0183d060f4ed5854d227d53511256"
+        consta val BASE_URL = "https://newsapi.org"
+    }
+}
